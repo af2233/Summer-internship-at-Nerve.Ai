63af2dd2ca00560571cecdd5b558a2826465818f
af2233 <a.fadeev.101@gmail.com>
2024-07-09 16:05:30 +0300

    models from DB

---
+++ models/Charger.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../sequelize');
+
+
+const Charger = sequelize.define('Charger', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  x: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  y: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  charged_batteries: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  discharged_batteries: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+}, {
+  // freezeTableName: true,
+  tableName: 'charger',
+  timestamps: false,
+});
+
+
+module.exports = Charger;
---
+++ models/Entity.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../sequelize');
+
+
+const Entity = sequelize.define('Entity', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  x: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  y: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  percent: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+  },
+  charged_batteries: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+  },
+  discharged_batteries: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  isHere: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+}, {
+  // freezeTableName: true,
+  tableName: 'entities',
+  timestamps: false,
+});
+
+
+module.exports = Entity;
